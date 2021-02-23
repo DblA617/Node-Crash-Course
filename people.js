02a71c9a7778df45b15b9c8cc0030ac1323051f7
Aarika Turner <turneraarika@gmail.com>
2021-02-23 08:08:48 -0500

    push node

---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ["Kiah", "Ron", "Drake", "Aarika", "Austin"];
+const ages = [19, 29, 32, 30, 18];
+console.log(people);
+
+module.exports = {
+    people,
+    ages
+}
---
+++ test.js
@@ -0,0 +1,9 @@
+// const name = "Patricia";
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+greet ('Soalice');
+greet ('Timothy');
+
